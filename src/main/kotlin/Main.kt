005059769d71b050d61296c2cc7441ee335c3219
Borigor <borigorr@gmail.com>
2023-04-18 19:40:46 +0300

    fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,9 +7,12 @@ fun calcCommission(
     amountCurrentMonth: Double = 0.0,
     cardType: String = "VKPay"
 ) = when {
+    cardType == "VKPay" && (amount > 15_000 || amountCurrentMonth > 40_000) -> throw LimitException()
+    cardType != "VKPay" && (amountCurrentMonth > 600_000 || amount > 150_000) -> throw LimitException()
     (cardType == "MasterCard" || cardType == "Maestro") && amountCurrentMonth > 75_000 -> amount * 0.006 - 20
     cardType == "Visa" || cardType == "Мир" -> {
         val commission = amount * 0.0075;if (commission < 35) 35 else commission
     }
     else -> 0.0
 }
+
